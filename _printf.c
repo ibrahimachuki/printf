77524c9890eb5c3a6fa6a47e2410e8e7ff8600ca
Otanga-Otieno <otieno.otanga@gmail.com>
2022-08-08 21:34:59 +0300

    Return number of characters printed

--- _printf.c
+++ _printf.c
@@ -2,6 +2,22 @@
 #include "main.h"
 
 /**
+ * string_length - return length of string
+ * @str: string
+ * Return: length of string
+ */
+int string_length(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * print_string - parse string
  * @str: string to print
  */
@@ -26,6 +42,7 @@ void print_string(char *str)
 int _printf(const char *format, ...)
 {
 	int count = 0;
+	int printed_chars = 0;
 	char ch, ch2;
 	va_list param_list;
 
@@ -33,10 +50,10 @@ int _printf(const char *format, ...)
 	while (format[count] != '\0')
 	{
 		ch = format[count];
-
 		if (ch != '%')
 		{
 			_putchar(ch);
+			printed_chars++;
 		}
 		else
 		{
@@ -46,20 +63,22 @@ int _printf(const char *format, ...)
 			if (ch2 == 'c')
 			{
 				_putchar(va_arg(param_list, int));
+				printed_chars++;
 			}
 			else if (ch2 == '%')
 			{
 				_putchar('%');
+				printed_chars++;
 			}
 			else
 			{
 				char *st = va_arg(param_list, char*);
 
+				printed_chars += string_length(st);
 				print_string(st);
 			}
 		}
 		count++;
 	}
-
-	return (0);
+	return (printed_chars);
 }
